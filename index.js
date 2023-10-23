358ba7734560b28980e0069dd11c171c32535004
Elias Artto <elias.artto.e@gmail.com>
2023-10-23 19:01:05 +0300

    port env added

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const app = express()
 app.get('/', (req, res) => {
     res.send()
 })
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
