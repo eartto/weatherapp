6a7523c3f1ea6cc622d2f6ef169e59d602491c62
Elias Artto <elias.artto.e@gmail.com>
2023-11-10 17:05:24 +0200

    delete req fix 2

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/api/cities', (req, res) => {
   })
 })
 
-app.delete('/api/cities', (req, res) => {
+app.delete('/api/cities/:id', (req, res) => {
   City.findOneAndDelete({},{"sort": { "_id": -1 }}).then(result => {
     console.log(result)
   })
